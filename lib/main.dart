6ae8323dcbae54f846f5ebd8a760464fd7cb4d97
K4RTOF3L <debilpolski15@gmail.com>
2020-04-04 19:24:12 +0200

    Initial project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+import 'core/bloc_delegate.dart';
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // inits all instances from one point
+  await di.init();
+
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
