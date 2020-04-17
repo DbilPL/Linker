a3bc74a8fab84f1f1fe709f9949c49cad6abcd1a
lavakalas <lavakalas@mail.ru>
2020-04-17 19:08:28 +0200

    Register Event 2

--- lib/features/authentication/presentation/bloc/authentication_bloc.dart
+++ lib/features/authentication/presentation/bloc/authentication_bloc.dart
@@ -1,18 +1,33 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:linker/core/errors/failure.dart';
+import 'package:linker/core/presentation/bloc/bloc.dart';
+import 'package:linker/features/authentication/domain/entities/user.dart';
+import 'package:linker/features/authentication/domain/usecases/register.dart';
 
 import './bloc.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc(this.register);
+
   @override
   AuthenticationState get initialState => InitialAuthenticationState();
+
+  final Register register;
 
   @override
   Stream<AuthenticationState> mapEventToState(
     AuthenticationEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is RegisterEvent) {
+      final result = await register(AuthenticationParams(
+          password: event.password, email: event.email, name: event.name));
+      yield result.fold((failure) {}, (success) {
+        return AuthenticationSuccessState(uid: success.uid);
+      });
+    }
   }
 }
--- lib/features/authentication/presentation/bloc/authentication_state.dart
+++ lib/features/authentication/presentation/bloc/authentication_state.dart
@@ -8,3 +8,11 @@ class InitialAuthenticationState extends AuthenticationState {
   @override
   List<Object> get props => [];
 }
+
+class AuthenticationSuccessState extends AuthenticationState {
+  final String uid;
+
+  AuthenticationSuccessState({this.uid});
+  @override
+  List<Object> get props => [uid];
+}
